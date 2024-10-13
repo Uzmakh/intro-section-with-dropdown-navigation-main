59750d31d676e39a0db5fb0bc9b81b2eb9b4a987
Uzma Khan <urbazoha@gmail.com>
2024-10-13 15:34:20 +0500

    set the CSS

--- script.js
+++ script.js
@@ -2,28 +2,33 @@
 const hamBurger = document.querySelector(".ham");
 const closeMenu = document.querySelector(".close-menu");
 const overLay = document.querySelector(".overlay");
-const mobileNav = document.querySelector(".mobile-nav");
+const mobileNav = document.querySelector(".desktop-nav");
 hamBurger.addEventListener("click", e => {
-  mobileNav.classList.add("show");
+   console.log("Menuopen is clicked")
+   e.preventDefault();
+  mobileNav.classList.add("active");
    overLay.style.display = "block";
-    document.querySelector("body").classList.add("lock");
+   closeMenu.classList.add("active");
+
 })
 closeMenu.addEventListener("click", e => {
-   mobileNav.classList.remove("show");
+   console.log("Menuclose is clicked")
+   e.preventDefault();
+   mobileNav.classList.remove("active");
    overLay.style.display = "none";
-    document.querySelector("body").classList.remove("lock");
+   closeMenu.classList.remove("active");
 })
 
 // Dropdown Menu Functionality on Mobile
-const dropdownItems = document.querySelectorAll(".dropdown-menu-item");
+// const dropdownItems = document.querySelectorAll(".dropdown-menu-item");
 
-dropdownItems.forEach((item) => {
-    item.addEventListener("click", function (e) {
-        e.preventDefault();
-        const dropdownContent = item.querySelector(".dropdown-content");
-        dropdownContent.classList.toggle("show");
+// dropdownItems.forEach((item) => {
+//     item.addEventListener("click", function (e) {
+//         e.preventDefault();
+//         const dropdownContent = item.querySelector(".dropdown-content");
+//         dropdownContent.classList.toggle("show");
 
-        const dropdownArrow = item.querySelector(".dropbtn");
-        dropdownContent.classList.contains("show") ? (dropdownArrow.src = "images/icon-arrow-up.svg") : (dropdownArrow.src = "images/icon-arrow-down.svg");
-    });
-})
+//         const dropdownArrow = item.querySelector(".dropbtn");
+//         dropdownContent.classList.contains("show") ? (dropdownArrow.src = "images/icon-arrow-up.svg") : (dropdownArrow.src = "images/icon-arrow-down.svg");
+//     });
+// })
